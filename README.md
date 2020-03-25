405c712a2e02acfa780a9b99b566513012954b08
LeeYoungMin <miniyoung37@gmail.com>
2020-03-25 15:34:41 +0900

    basic interface concept

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ## TypeScript
 
 basic TypeScript concept study
+
+1. setup typescript : yarn add typescript
+2. yarn add tsc-watch --dev
+3. add .gitignore
+4. Interface
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,26 @@ const name = "lee",
   age = 24,
   gender = "male";
 
-const sayHi = (name: string, age: number, gender?: string): string => {
-  return `Hello ${name}, you are ${age}, you are a ${gender}`;
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "min",
+  age: 22,
+  gender: "male"
 };
 
-console.log(sayHi(name, age, gender));
+// const sayHi = (name: string, age: number, gender?: string): string => {
+//   return `Hello ${name}, you are ${age}, you are a ${gender}`;
+// };
+
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
+};
+
+console.log(sayHi(person));
 
 export {}; // 모듈로 생성
